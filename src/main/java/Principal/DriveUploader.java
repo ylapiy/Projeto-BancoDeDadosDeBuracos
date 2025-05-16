c4107087f9b8def1c4c55b7d1c5fad70e5004f50
ylapiy <ygorgivagooo@gmail.com>
2025-05-16 14:40:29 -0300

    Adicionado integração com googleDrive

--- src/main/java/Principal/DriveUploader.java
+++ src/main/java/Principal/DriveUploader.java
@@ -1,9 +1,20 @@
+package Principal;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
@@ -12,13 +23,6 @@ import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
 
 /* class to demonstrate use of Drive files list API */
 public class DriveUploader {
@@ -39,9 +43,8 @@ public class DriveUploader {
    * Global instance of the scopes required by this quickstart.
    * If modifying these scopes, delete your previously saved tokens/ folder.
    */
-  private static final List<String> SCOPES =
-      Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
-  private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+  private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
+  private static final String CREDENTIALS_FILE_PATH = "/Principal_resources/secrets.json";
 
   /**
    * Creates an authorized Credential object.
@@ -50,15 +53,13 @@ public class DriveUploader {
    * @return An authorized Credential object.
    * @throws IOException If the credentials.json file cannot be found.
    */
-  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-      throws IOException {
-    // Load client secrets.
+  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)  throws IOException {
+		
     InputStream in = DriveUploader.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
     if (in == null) {
       throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
     }
-    GoogleClientSecrets clientSecrets =
-        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
     // Build flow and trigger user authorization request.
     GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
@@ -66,32 +67,98 @@ public class DriveUploader {
         .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
         .setAccessType("offline")
         .build();
-    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).setCallbackPath("/Callback").build();
     Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    //returns an authorized Credential object.
     return credential;
   }
 
-  public static void main(String... args) throws IOException, GeneralSecurityException {
-    // Build a new authorized API client service.
+  /**
+   * Uploads a file to Google Drive.
+   *
+   * @param localFilePath The path to the local file to upload.
+   * @param mimeType      The MIME type of the file.
+   * @param driveFileName The name to give the file on Google Drive.
+   * @throws IOException              If an error occurs during the upload.
+   * @throws GeneralSecurityException If an error occurs with security.
+   */
+  public void post(String localFilePath, String mimeType, String driveFileName)  throws IOException, GeneralSecurityException {
+	
     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
     Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
         .setApplicationName(APPLICATION_NAME)
         .build();
 
-    // Print the names and IDs for up to 10 files.
-    FileList result = service.files().list()
-        .setPageSize(10)
-        .setFields("nextPageToken, files(id, name)")
+    // Create file metadata
+    File fileMetadata = new File();
+    fileMetadata.setName(driveFileName);
+    fileMetadata.setParents(Collections.singletonList("1CuJLqPsk0rc3poQyDlhyt5dtSSVDs6ys"));
+
+    // Specify the file path and MIME type
+    
+    java.io.File filePath = new java.io.File(localFilePath);
+    FileContent mediaContent = new FileContent(mimeType, filePath);
+
+    // Upload
+    File uploadedFile = service.files().create(fileMetadata, mediaContent)
+        .setFields("id, name")
         .execute();
-    List<File> files = result.getFiles();
-    if (files == null || files.isEmpty()) {
-      System.out.println("No files found.");
-    } else {
-      System.out.println("Files:");
-      for (File file : files) {
-        System.out.printf("%s (%s)\n", file.getName(), file.getId());
-      }
-    }
+
+    System.out.printf("Archive sent: %s (ID: %s)\n", uploadedFile.getName(), uploadedFile.getId());
   }
+
+
+//  public void get() throws IOException, GeneralSecurityException {
+//    // Build a new authorized API client service.
+//    final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+//    Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+//        .setApplicationName(APPLICATION_NAME)
+//        .build();
+//    
+//    FileList result = service.files().list()
+//    	    .setQ("'1CuJLqPsk0rc3poQyDlhyt5dtSSVDs6ys' in parents ")
+//    	    .setCorpora("allDrives")   
+//    	    .setPageSize(10)
+//    	    .setFields("nextPageToken, files(id, name)")
+//    	    .setSupportsAllDrives(true)            
+//    	    .setIncludeItemsFromAllDrives(true)    
+//    	    .execute();
+//    
+//    List<File> files = result.getFiles();
+//    if (files == null || files.isEmpty()) {
+//      System.out.println("No files found.");
+//    } else {
+//      System.out.println("Files:");
+//      for (File file : files) {
+//        System.out.printf("%s (%s)\n", file.getName(), file.getId());
+//      }
+//    }
+//  }
+
+  public void get() throws IOException, GeneralSecurityException {
+	    final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+	    Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+	        .setApplicationName(APPLICATION_NAME)
+	        .build();
+
+	    String folderId = "1CuJLqPsk0rc3poQyDlhyt5dtSSVDs6ys";
+
+	    FileList result = service.files().list()
+	        .setQ("'" + folderId + "' in parents and trashed = false")
+	        .setPageSize(20)
+	        .setFields("files(id, name, mimeType)")
+	        .execute();
+
+	    List<File> files = result.getFiles();
+	    if (files == null || files.isEmpty()) {
+	        System.out.println("Nenhum arquivo encontrado.");
+	    } else {
+	        System.out.println("Arquivos encontrados:");
+	        for (File file : files) {
+	            System.out.printf("%s (%s) [%s]\n", file.getName(), file.getId(), file.getMimeType());
+	        }
+	    }
+	}
+  
+  
+  
 }
--- src/main/java/Principal/Main.java
+++ src/main/java/Principal/Main.java
@@ -1,34 +1,52 @@
 package Principal;
 
-import java.util.Scanner;
-
 public class Main {
 
 	 public static void main(String[] args) {
 
-		 Scanner Entrada = new Scanner(System.in);
+		//  Scanner Entrada = new Scanner(System.in);
 		 
-		 System.out.println("Digite o caminho do banco de dados");
-		 String url = Entrada.next();
+		//  System.out.println("Digite o caminho do banco de dados");
+		//  String url = Entrada.next();
 		 
-		 System.out.println("Disite o usuario do banco de dados");
-		 String usuario = Entrada.next();
+		//  System.out.println("Disite o usuario do banco de dados");
+		//  String usuario = Entrada.next();
 		 
-		 System.out.println("Digite a senha do banco de dados");
-		 String senha = Entrada.next();
+		//  System.out.println("Digite a senha do banco de dados");
+		//  String senha = Entrada.next();
 		 
-		 System.out.println("Digite a API_KEY da geografia reversa");
-		 String KEY = Entrada.next();
+		//  System.out.println("Digite a API_KEY da geografia reversa");
+		//  String KEY = Entrada.next();
 		 
-		 Principal.GeografiaReversa.ColocarKEY(KEY);
-		 BancoDeDados teste = new BancoDeDados(url,usuario,senha);
+		//  Principal.GeografiaReversa.ColocarKEY(KEY);
+		//  BancoDeDados teste = new BancoDeDados(url,usuario,senha);
 		  
-		 teste.ConcetarPost();
+		//  teste.ConcetarPost();
 		  
-		 teste.printarTabela();
+		//  teste.printarTabela();
+		
 		  
+//		try {
+//		DriveUploader drive = new DriveUploader();
+//	drive.post("src/main/java/Principal_resources/20560981.jpeg", "image/jpeg", "20560981.jpeg");
+//	} catch (IOException | GeneralSecurityException e) {
+//	 e.printStackTrace();  		}
+	 
 		 
-		 /*
+//		 try {
+//			 DriveUploader drive = new DriveUploader();
+//			 drive.get();
+			 
+//		 }catch(IOException | GeneralSecurityException e) {
+//   		e.printStackTrace();
+//  		 }
+			 
+			 
+			 
+		 }
+		 
+		 
+		/*
 		 
 		 Principal.GeografiaReversa.RuaeBairro(Principal.Metadados.PegarDadoGPSLatitude(caminho),Principal.Metadados.PegarDadosGPSLongitude(caminho));
 		 Principal.Metadados.PegarDadoData(caminho);
@@ -37,6 +55,6 @@ public class Main {
 
          */
 		  
-}}
+}
 	
 
